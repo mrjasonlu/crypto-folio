edba5abbe787cdf52da0626bdf5988416ab2c3b4
Jason Lu <jasonlu@live.com.au>
2023-10-30 17:06:49 +1100

    add path alias

--- app/babel.config.js
+++ app/babel.config.js
@@ -1,4 +1,27 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['relay'],
+  plugins: [
+    'relay',
+    [
+      'module-resolver',
+      {
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.ios.jsx',
+          '.android.jsx',
+          '.js',
+          '.jsx',
+          '.json',
+          '.ts',
+          '.tsx',
+        ],
+        root: ['.'],
+        alias: {
+          '@src': './src',
+          '@assets': './assets',
+        },
+      },
+    ],
+  ],
 };
